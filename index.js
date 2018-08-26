7a68f0840eeef9518136e21b81deebc1162392a8
simoberny <simone.bernabe@studenti.unitn.it>
2018-08-26 22:44:41 +0200

    cleaner version

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const isFlag = (arg) => arg && !isNumber(arg) && arg.startsWith('--')
 const isNumber = (num) => parseInt(num) === +num
+const parseValue = (value) => isNumber(value) ? parseInt(value) : value
 
-module.exports = argsToJSON = (args) => {
-    if(!Array.isArray(args)) args = args.split(" ")
+const argsToJSON = (args) => {
+  if (!Array.isArray(args)) args = args.split(' ')
 
-    let json = {}
+  let json = {}
 
-    args.forEach((arg, i) => {  
-        const next = args[i+1]
-        if(isFlag(arg)){
-            const key = arg.substr(2)
-            if(!next || isFlag(next)) json[key] = true
-            else if(isNumber(next)) json[key] = parseInt(next)
-            else if(!isFlag(next)){
-                if(json[key]) json[key] = [...[json[key]], next].reduce((acc, val) => acc.concat(val), [])
-                else json[key] = next
-            }
-        }
-    })
+  args.forEach((arg, i) => {
+    const next = args[i + 1]
 
-    return json
+    if (isFlag(arg)) {
+      const key = arg.substr(2)
+      if (!next || isFlag(next)) json[key] = true
+      else if (!isFlag(next)) {
+        json[key] = json[key] ? [].concat(json[key], parseValue(next)) : parseValue(next)
+      }
+    }
+  })
+
+  return json
 }
+
+module.exports = argsToJSON
